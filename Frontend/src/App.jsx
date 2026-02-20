7dec7d48587915d13cfa6d7cb7793abba60a11fc
melaniendikubwimana-cloud <melaniendikubwimana@gmail.com>
2026-02-20 16:50:38 +0200

    Updated home menu

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
-
+import { Route, Routes } from 'react-router-dom'
+import Cart from './Components/Pages/Cart/Cart'
+import Home from './Components/Pages/Cart/Home/Home'
+import Placeholder from './Components/Pages/PlaceOrder/Placeorder'
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar/>
+      <Routes>
+          
+        <Route path='/' element={<Home/>} />
+         <Route path='/Cart' element={<Cart/>} />
+         <Route path='/order' element={<Placeholder/>} />
+      </Routes>
       
     </div>
   )
---
+++ Frontend/src/Components/Pages/PlaceOrder/Placeorder.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Placeorder.css'
+const Placeholder = () => {
+  return (
+    <div className='placeholder'>
+      
+    </div>
+  )
+}
+
+export default Placeholder
