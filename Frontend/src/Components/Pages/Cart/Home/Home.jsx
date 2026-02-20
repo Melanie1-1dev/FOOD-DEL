aa2d4b434f70e8c95c8cc5ed203fbca1ed7a41dd
melaniendikubwimana-cloud <melaniendikubwimana@gmail.com>
2026-02-20 21:35:46 +0200

    update images to chosenby customers

--- Frontend/src/Components/Pages/Cart/Home/Home.jsx
+++ Frontend/src/Components/Pages/Cart/Home/Home.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Home.css'
 import Header from '../../../Header/Header'
+import ExploreMenu from '../../../Exploremenu/Exploremenu'
 const Home = () => {
+const All="All items";
+    const [category,setCategory] = useState(All);
   return (
     <div className='home'>
         <Header/>
+        <ExploreMenu category={category} setCategory={setCategory}/>
+        <ExploreMenu/>
     </div>
   )
 }
