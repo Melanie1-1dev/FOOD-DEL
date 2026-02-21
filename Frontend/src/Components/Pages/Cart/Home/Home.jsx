a8d0f14ff15cbe56457a6d1afb67c9bc6dee3c74
melaniendikubwimana-cloud <melaniendikubwimana@gmail.com>
2026-02-21 09:13:40 +0200

    updated website

--- Frontend/src/Components/Pages/Cart/Home/Home.jsx
+++ Frontend/src/Components/Pages/Cart/Home/Home.jsx
@@ -2,14 +2,16 @@ import React, { useState } from 'react'
 import './Home.css'
 import Header from '../../../Header/Header'
 import ExploreMenu from '../../../Exploremenu/Exploremenu'
+import FoodDisplay from '../../../FoodDisplay/FoodDisplay'
 const Home = () => {
 const All="All items";
-    const [category,setCategory] = useState(All);
+    const [category,setCategory] = useState("All");
   return (
     <div className='home'>
         <Header/>
         <ExploreMenu category={category} setCategory={setCategory}/>
-        <ExploreMenu/>
+        <FoodDisplay category={category}/>
+        
     </div>
   )
 }
