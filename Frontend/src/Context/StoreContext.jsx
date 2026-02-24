c4210a13d0d740f9bb6194e7e589640a20dcc20c
melaniendikubwimana-cloud <melaniendikubwimana@gmail.com>
2026-02-24 09:39:25 +0200

    updated

--- Frontend/src/Context/StoreContext.jsx
+++ Frontend/src/Context/StoreContext.jsx
@@ -6,7 +6,7 @@ const StoreContextProvider = (props) =>{
        
     const[cartItems,setCartItems] =useState({});
 
-    const addToCart = () => {
+    const addToCart = (itemId) => {
         if(!cartItems[itemId]){
             setCartItems((prev)=>({...prev,[itemId]:1}))
         }
