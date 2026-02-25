d3c75181482f24f680f745dbae09ce99a3d96f01
melaniendikubwimana-cloud <melaniendikubwimana@gmail.com>
2026-02-25 21:25:12 +0200

    Updated

--- Frontend/src/Components/LoginPopup/LoginPopUp.jsx
+++ Frontend/src/Components/LoginPopup/LoginPopUp.jsx
@@ -9,8 +9,14 @@ const LoginPopup = ({setShowLogin}) => {
         <form className="login-popup-container">
             <div className="login-popup-title">
                 <h2>{currState}</h2>
-                <img onClick={()=>} src={assets.cross_icon} alt="" />
+                <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt="" />
             </div>
+            <div className="login-popup-inputs">
+                <input type="text" placeholder='Your name' required/>
+                <input type="email" placeholder='Your email' required />
+                <input type="password" placeholder='Password' required/>
+            </div>
+            <button>{currState==="Sign Up"?"Create account":"Login"}</button>
         </form>
       
     </div>
