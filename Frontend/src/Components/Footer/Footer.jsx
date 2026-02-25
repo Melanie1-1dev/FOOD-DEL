47d8b8f0c81d58578592c8a4fbfcbe4dbe5950cf
melaniendikubwimana-cloud <melaniendikubwimana@gmail.com>
2026-02-25 08:28:36 +0200

    updated

--- Frontend/src/Components/Footer/Footer.jsx
+++ Frontend/src/Components/Footer/Footer.jsx
@@ -18,13 +18,27 @@ const Footer = () => {
             </div>
             
             <div className="footer-content-center">
+                <h2>COMPANY </h2>
+                <ul>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Delivery</li>
+                    <li>Privacy policy</li>
+                </ul>
                 
             </div>
             <div className="footer-content-right">
+                <h2>GET IN TOUCH</h2>
+                <ul>
+                    <li>+1-212-456-7890</li>
+                    <li>contact@tomato.com</li>
+                </ul>
 
             </div>
             
         </div>
+        <hr/>
+        <p className="footer-copyright">Copyright 2024 &copy; Tomato.com - All Rights are Reserved.</p>
       
     </div>
   )
