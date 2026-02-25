b4d9c536d1fff4bf066deacd939f363fc0bdd2cc
melaniendikubwimana-cloud <melaniendikubwimana@gmail.com>
2026-02-25 21:30:57 +0200

    Updated

--- Frontend/src/Components/LoginPopup/LoginPopUp.jsx
+++ Frontend/src/Components/LoginPopup/LoginPopUp.jsx
@@ -3,7 +3,7 @@ import './LoginPopUp.css'
 import { assets } from '../../assets/assets'
 
 const LoginPopup = ({setShowLogin}) => {
-    const [currState,setCurrState] = useState("Sign Up")
+    const [currState,setCurrState] = useState("Login")
   return (
     <div className='login-popup'>
         <form className="login-popup-container">
@@ -12,11 +12,16 @@ const LoginPopup = ({setShowLogin}) => {
                 <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt="" />
             </div>
             <div className="login-popup-inputs">
-                <input type="text" placeholder='Your name' required/>
+                {currState==="Login"?<></>:
+                <input type="text" placeholder='Your name' required/>}
                 <input type="email" placeholder='Your email' required />
                 <input type="password" placeholder='Password' required/>
             </div>
             <button>{currState==="Sign Up"?"Create account":"Login"}</button>
+            <div className="login-popup-condition">
+                <input type="checkbox" required/>
+                <p>By continuing, I agree to the terms of use & privacy policy.</p>
+            </div>
         </form>
       
     </div>
