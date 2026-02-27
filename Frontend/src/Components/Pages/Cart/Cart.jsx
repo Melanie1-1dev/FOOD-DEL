1fd455a6c5dbde3605ff313743ac3148467b3de3
melaniendikubwimana-cloud <melaniendikubwimana@gmail.com>
2026-02-27 14:02:53 +0200

    updated

--- Frontend/src/Components/Pages/Cart/Cart.jsx
+++ Frontend/src/Components/Pages/Cart/Cart.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Cart.css'
+import { StoreContext } from '../../../Context/StoreContext'
 const Cart = () => {
+
+  const{cartItems,food_list,removeFromCart} = useContext(StoreContext)
   return (
-    <div>
+    <div className='cart'>
+      <div className="cart-items">
+        <div className="cart-items-title">
+          <p>Items</p>
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
+        </div>
+      </div>
       
     </div>
   )
